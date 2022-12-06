ee55bd47394da86f0a9a40ce9b95ed907e6c29be
delmashajan <delmashajan@gmail.com>
2022-12-06 22:13:50 +0530

    final commit

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-product',
@@ -16,10 +17,31 @@ export class AddProductComponent {
   sellerName=""
   distributorName=""
 
+  constructor(private api:ApiService){}
+
   readValue=()=>{
     let data:any={"productCode":this.productCode,"productName":this.productName,"manufacturingDate":this.manufacturingDate,
   "expiryDate":this.expiryDate,"brand":this.brand,"prize":this.prize,"sellerName":this.sellerName,"distributorName":this.distributorName}
   console.log(data)
+
+  this.api.addProduct(data).subscribe(
+    (response:any)=>{
+      console.log(response)
+      if(response.status=="success"){
+        alert("added successfully")
+        this.productCode=""
+        this.productName=""
+        this.manufacturingDate=""
+        this.expiryDate=""
+        this.brand=""
+        this.prize=""
+        this.sellerName=""
+        this.distributorName
+      }else{
+        alert("wrong")
+      }
+    }
+  )
   }
 
 }
